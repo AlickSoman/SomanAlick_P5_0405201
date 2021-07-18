7b816eb9f4f379b83fee9de4c9194de91a46dd3e
Alick SOMAN <alick.soman@hotmail.fr>
2021-07-18 17:35:38 +0200

    V18.07.2021

--- front-end/js/panier.js
+++ front-end/js/panier.js
@@ -17,7 +17,7 @@ console.log(selectionElements);
 // si le panier est vide : afficher le panier est vide
 if(produitEnregistrerDansLocalStorage ===null){
     const panierVide = `
-    <div>
+    <div id="panier_vide">
         <div>Le panier est vide </div>
     </div>`;
 
@@ -34,6 +34,8 @@ else{
     let structureProduitPanier = [];
     var i=0; // injection des varriations de couleurs
     var classeCouleur = '';
+    var totalPanier = 0; // cration de la var totalPanier
+    var totalQty = 0;  // cration de la var totalQty
     for(idProduit in produitEnregistrerDansLocalStorage){
         var url = "http://localhost:3000/api/cameras/" + idProduit; //+ id pour récupé un article bien specifique
         //recuperer les articles
@@ -44,15 +46,17 @@ else{
         else {
             classeCouleur = "impair";
         }
-        
-        var qty = produitEnregistrerDansLocalStorage[idProduit];
+        //récuper les valeur des var déclarer plus haut ligne 35 à 38
+        var qty = parseInt (produitEnregistrerDansLocalStorage[idProduit]);
+
         let article = await getArticle(url);
         var articlePrice = article.price/100;
         var totalArticle = qty*article.price/100;
-        
+        totalPanier += totalArticle;
+        totalQty += qty;
 
 
-
+        //injection des données dans la pagne html
         console.log(totalArticle);
         structureProduitPanier = structureProduitPanier + `
             <div class="container-recapitulatif ${classeCouleur}">
@@ -61,35 +65,123 @@ else{
                
                 <div> Quantité : ${qty}</div>
                 <div> prix : ${totalArticle} €</div>
+                <div id="sup-1-IdProduit"> <img src="img/cart-plus-solid.svg" alt="image"></div>
                 
             </div>
             `;
+
+            //addEnventlistner sur mon btn supprimer 1 alarticle
+
+
+
+
             i++;
 
-            // Panier  : doit contabiliser touts les produit dans le localStorage et les afficher
-
-            // structureProduitPanierFinal = structureProduitPanierFinal + `
-        //    <div class="panierFinal ${classeCouleur}">
-                
-        //         <div>Prix : ${articlePrice}*${qty}€ </div>
-        //         <div> QNombres total d'articles : ${qty}</div>
-        //         <div> prix total : ${totalArticle} €</div>
-        //         <div><button>Validé</button></div>
-        //     </div>
-        //    `; 
 
 
             
 }
 
+        //     Panier  : doit contabiliser touts les produit dans le localStorage et les afficher
+        //injection des données dans la pagne html
+            structureProduitPanier = structureProduitPanier + `
+           <div class="panierFinal">
+                <div>Nombres total d'articles : ${totalQty}</div>
+                <div>prix total : ${totalPanier} €</div>
+            </div>
 
+
+            <form action="/ma-page-de-traitement" method="post">
+            <div>
+                <label for="firstName">Nom :</label>
+                <input type="text" id="firstName" name="user_firstName" required>
+            </div>
+            <div>
+                <label for="lastName">Prénom :</label>
+                <input type="text" id="lastName" name="user_lastName" required>
+            </div>
+
+            <div>
+                <label for="mail">email :</label>
+                <input type="email" id="email" name="user_email" required>
+            </div>
+    
+            <div>
+                <label for="adresse">Nom de rue :</label>
+                <input type="text" id="adresse" name="user_adresse" required>
+            </div>
+    
+            <div>
+                <label for="adresseNumb">N° de rue :</label>
+                <input type="number" id="adresseNumb" name="user_adresseNumb" required>
+            </div>
+    
+            <div>
+                <label for="city">Ville :</label>
+                <input type="text" id="city" name="user_city" required>
+            </div>
+    
+            <div>
+                <label for="codeP">Code postal :</label>
+                <input type="number" id="codeP" name="user_codeP" required>
+            </div>
+    
+            <button id="btn_commande" type="submit">Envoyer ma Commande</button>
+        </form>
+           `; 
             //alors en injecte la structure html dans le panier
             selectionElements.innerHTML = structureProduitPanier;
+        
 
+             //selection du btn envoi de la commande========================
+             var btnEnvoyerLaCommande = document.querySelector("#btn_commande")
+            //  console.log(btnEnvoyerLaCommande)
+
+            // ---------------------addEentListner-------------------------
+             btnEnvoyerLaCommande.addEventListener("click",(e) => {
+                // e.preventDefault(); // annule le conportement par défaut
+
+
+
+             //enregistrement du formulaire dans le local sorage
+             localStorage.setItem('fistName', document.querySelector("#firstName").value);
+             localStorage.setItem('lastName', document.querySelector("#lastName").value);
+             localStorage.setItem('email', document.querySelector("#email").value);
+             localStorage.setItem('adresse', document.querySelector("#adresse").value);
+             localStorage.setItem('adresseNumb', document.querySelector("#adresseNumb").value);
+             localStorage.setItem('city', document.querySelector("#city").value);
+             localStorage.setItem('codeP', document.querySelector("#codeP").value);
+
+
+
+            
+            // console.log("Element du formulaire :" ('Nom', document.querySelector("#firstname").value))
+
+            //Mettre les values et produits du pannier dans un objet pour l'envoi vers le serveur
+             const formulaire = {
+                  firstName: localStorage.setItem('firstName'),
+                  lastName: localStorage.setItem('lastName'),
+                  email: localStorage.setItem('email'),
+                  adresse: localStorage.setItem('adresse'),
+                  adresseNumb:localStorage.setItem('adresseNumb'),
+                  city: localStorage.setItem('city'),
+                  codeP: localStorage.setItem('codeP'),
+             }
+             console.log(formulaire)
+
+             });
+              
 }
 
+
+
+
+
 })()
-//get acticles
+
+
+
+//get acticles : je recupere les informations des article selectionner
 
 async function getArticle(url){
     try {
--- front-end/js/product.js
+++ front-end/js/product.js
@@ -85,7 +85,7 @@ function displayArticle(article){
    lentilles.appendChild(option);
    option.textContent = article.lenses[i];
    option.value = article.lenses[i];
-    console.log(option)
+//    console.log(option)
   }
 
 // Fin affichage des option 
@@ -94,7 +94,7 @@ function displayArticle(article){
 
     //ici ajout du bouton "ajouter au panier"
     let boutonAjouter = document.getElementById("btn_envoyer");
-    console.log(boutonAjouter);
+//    console.log(boutonAjouter);
     boutonAjouter.addEventListener("click",function(){
         //ajouter ici les fonction néssessaire à la function 
         localStorage.setItem('ajouterAuPanier', 'btn_envoiyer');
@@ -106,22 +106,36 @@ function displayArticle(article){
         //JSON.parse c'est pour convertir les données au format JSON qui sont dans le local storage en objet Javascript
         console.log(produitEnregistrerDansLocalStorage);
         
-        if(produitEnregistrerDansLocalStorage){
-            let qty_article = parseInt (produitEnregistrerDansLocalStorage[id_article]); 
-            qty_article +=1;
-            // ajouter la nouvelle qty
-            produitEnregistrerDansLocalStorage[id_article]  = qty_article;
+        if(produitEnregistrerDansLocalStorage){ // si on a un panier
+            let checkCart = 0; // on surveille si on trouve l'article dans le panier
+            for (var cartArticle in produitEnregistrerDansLocalStorage) { // pour chaque article du panier
+                console.log(cartArticle);
+                if (id_article.localeCompare( cartArticle) == 0) { // si l'id correspond a larticle aue je veux ajouter
+                    let qty_article = parseInt (produitEnregistrerDansLocalStorage[id_article]);  // je recupère la quantite
+                    qty_article +=1; // j'augmente la quantite
+                    // ajouter la nouvelle qty
+                    produitEnregistrerDansLocalStorage[id_article]  = qty_article; // je stocke la nouvelle qty dans le panier
+                    localStorage.setItem('panier', JSON.stringify(produitEnregistrerDansLocalStorage)); //j'enregistre le panier
+                    console.log(produitEnregistrerDansLocalStorage);
+                    checkCart = 1;     //je note aue j'ai trouvé l'article
+                }
+            }
+            if (checkCart == 0) { // si je n'ai pas trouvé l'article
+            //console.log(id_article);
+            produitEnregistrerDansLocalStorage[id_article]  = "1"; // j'ajoute ce nouvel article au panier
             localStorage.setItem('panier', JSON.stringify(produitEnregistrerDansLocalStorage));
             console.log(produitEnregistrerDansLocalStorage);
 
+            }
+
         }
         else{
-            
-            console.log(id_article);
+            //console.log(id_article);
             let produitEnregistrerDansLocalStorage = {};
-            produitEnregistrerDansLocalStorage[id_article]  = +1;
+            produitEnregistrerDansLocalStorage[id_article]  = "1";
             localStorage.setItem('panier', JSON.stringify(produitEnregistrerDansLocalStorage));
-        //     console.log(produitEnregistrerDansLocalStorage);
+            console.log(produitEnregistrerDansLocalStorage);
+            
         }
 
 
